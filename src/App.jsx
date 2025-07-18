23320d5b6c1ffa0abd83f8b11ec42f025fef87ee
Abhinav Singh Negi <abhinavsinghnegi56@gmail.com>
2025-07-18 16:46:19 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div className='content'>
+    <Nav/>
+      
+    </div> 
+  )
+}
+
+export default App
