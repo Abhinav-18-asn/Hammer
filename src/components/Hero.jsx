ce6090bcd22b8d3ba503326260818a4fc85b22a8
Abhinav Singh Negi <abhinavsinghnegi56@gmail.com>
2025-07-21 07:10:45 +0530

    u

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='relative '>
 {/*Earphones */}
-     <div className='ehs  w-[20rem] h-[20rem] border-3 absolute top-[12rem] rounded-full border-zinc-700'>
+     <div className='ehs  w-[20rem] h-[20rem] border-3 absolute top-[5.7rem] rounded-full border-zinc-700'>
         <div className=' w-full h-full'>
               <div className='earHero w-[10rem] h-[10rem] rounded-full absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] border-zinc-700 border '>
                  {/*image anime*/}
@@ -17,9 +17,9 @@ const Hero = () => {
     {/*Headphones*/}
   
         <div className=' w-full h-full'>
-              <div className='headHero w-[10rem] h-[10rem] rounded-full absolute -bottom-[32rem] left-[50%] -translate-x-[50%] -translate-y-[50%]  border'>
+              <div className='headHero w-[10rem] h-[10rem] rounded-full absolute -bottom-[25.7rem] left-[50%] -translate-x-[50%] -translate-y-[50%]  border'>
                  {/*image anime*/}
-        <img src="/Headphone.png" alt="" className='ring2 absolute w-[20rem] left-[35%] top-[4rem] translate-x-[50%]' />
+        <img src="/Headphone.png" alt="" className='ring2 absolute w-[8rem] scale-70 left-[58%] top-[4rem] translate-x-[50%]' />
                   <p className='ring2T absolute text-center left-[50%] -translate-x-[50%] top-8 text-sm w-full px-2 leading-tight'>Premium Wireless Headphones with Noise Cancellation</p>
               </div>
         </div>
@@ -27,7 +27,7 @@ const Hero = () => {
     {/*Speakers*/}
   
         <div className=' w-full h-full'>
-              <div className='speakHero w-[10rem] h-[10rem] rounded-full absolute top-[22rem] left-[50%] -translate-x-[50%] -translate-y-[50%] border-zinc-700 border'>
+              <div className='speakHero w-[10rem] h-[10rem] rounded-full absolute top-[15.7rem] left-[50%] -translate-x-[50%] -translate-y-[50%] border-zinc-700 border'>
                  {/*image anime*/}
         <img src="/Speaker.png" alt="" className='ring3 absolute w-[3rem] -left-[4rem] top-[5rem] rotate-90 -translate-x-[50%]' />
                   <p className='ring3T absolute text-center left-[50%] -translate-x-[50%] top-10 text-sm w-full px-2'>Fill your room with immersive sound.
--- src/components/Moto.jsx
+++ src/components/Moto.jsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion'
 import Hero from './Hero'
 const Moto = () => {
   return (
-    <div className='motoCont w-[20rem]  absolute left-1/2 top-[50%] -translate-x-[50%] -translate-y-[200%]'>
+    <div className='motoCont w-[20rem]  absolute left-1/2 top-[75%] -translate-x-[50%] -translate-y-[200%]'>
       <h1 className='moto text-2xl text-center font-bold '>SO<span className='span'>UND T</span>HAT SPEAKS YOUR SOUL.</h1>
       <Hero/>
     </div>
